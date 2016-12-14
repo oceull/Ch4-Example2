d4dad7ab16347238328911574729de03cb081963
Luc van Roosmalen <l.l.vanroosmalen@student.tudelft.nl>
2016-12-14 15:26:55 +0100

    prevented possibility to add an empty comment

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var main = function(){
 	"use strict";
 
 	$(".comment-input button").on("click", function (event) {
-		var $new_comment = $("<p>"),
-			comment_text = $(".comment-input input").val();
+		var $new_comment;
 
-		$new_comment.text(comment_text);
-		
-		$(".comments").append($new_comment);
+		if($(".comment-input input").val() !==""){
+			$new_comment=$("<p>").text($(".comment-input input").val());
+			$(".comments").append($new_comment);
+		}
 	});
 };
 
